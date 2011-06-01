2029fdf070887e21610923a90a7c116a00ad1e1d
pbertera <p.bertera@gmail.com>
2011-06-01 18:34:25 +0200

    * new build system * packeges hierarchy

--- shell.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-rm -rf setenv-addictional
-/bin/bash --noprofile --rcfile ./setenv
---
+++ src/config.mk
@@ -0,0 +1,15 @@
+# configuration suff:
+# TODO in ./configure script
+
+export CM_LIBDIR=/opt/cm/lib
+export CM_BINDIR=/opt/cm/bin
+export CM_DBDIR=/opt/cm/db
+export BUILDDIR=/tmp/cmbb
+export COMPLETION_DIR=/etc/bash_completion.d/
+export CM_OPERATOR_USER=cm
+export CM_OPERATOR_HOME=/home/$(CM_OPERATOR_USER)
+export CM_OPERATOR_LIBEXECDIR=/opt/cm/operator-libexec
+export CM_CONFIGURE_USER=cmsu
+export CM_CONFIGURE_HOME=/home/$(CM_CONFIGURE_USER)
+export CM_CONFIGURE_LIBEXECDIR=/opt/cm/configure-libexec
+export CM_CONFIGURE_PWD=ciccio

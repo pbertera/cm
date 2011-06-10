57920c744737c95fc0a77aaefb68cc25f9f6139d
pbertera <p.bertera@gmail.com>
2011-06-10 18:44:49 +0200

    * added "interfaces ethernet add" command

---
+++ utils/rename_command.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+[ -z $1 ] && ( echo "ERROR: command name is required"; exit -1)
+[ -z $2 ] && ( echo "ERROR: command destination is required"; exit -1)
+
+[ -e $1 ] || (echo "ERROR: $1 not exist"; exit -1)
+
+# name  _name_complete  _name_help  _name_nocomplete  _name_priority  _name_syntax
+mv $1 $2
+mv _$1_complete _$2_complete
+mv _$1_help _$2_help
+mv _$1_priority _$2_priority
+mv _$1_syntax _$2_syntax
+[ -e _$1_nocomplete ] && mv _$1_nocomplete _$2_nocomplete

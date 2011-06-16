9a26fcd8172acea331f4b2d94b515270c84d1a0e
pbertera <p.bertera@gmail.com>
2011-06-16 17:27:38 +0200

    * ask for password during installation

--- Makefile
+++
@@ -1,8 +0,0 @@
-.PHONY: src
-src:
-	cd src && make all
-clean:
-	cd src && make clean
-
-all: 
-	make src
--- src/config.mk
+++ src/config.mk
@@ -1,15 +1,14 @@
 # configuration suff:
 # TODO in ./configure script
 
-export CM_LIBDIR=/opt/cm/lib
-export CM_BINDIR=/opt/cm/bin
-export CM_DBDIR=/opt/cm/db
+export CM_LIBDIR=/opt1/cm/lib
+export CM_BINDIR=/opt1/cm/bin
+export CM_DBDIR=/opt1/cm/db
 export COMPLETION_DIR=/etc/bash_completion.d/
-export CM_OPERATOR_USER=cm
+export CM_OPERATOR_USER=cm1
 export CM_OPERATOR_HOME=/home/$(CM_OPERATOR_USER)
-export CM_OPERATOR_LIBEXECDIR=/opt/cm/operator-libexec
-export CM_CONFIGURE_USER=cmsu
+export CM_OPERATOR_LIBEXECDIR=/opt1/cm/operator-libexec
+export CM_CONFIGURE_USER=cmsu1
 export CM_CONFIGURE_HOME=/home/$(CM_CONFIGURE_USER)
-export CM_CONFIGURE_LIBEXECDIR=/opt/cm/configure-libexec
-export CM_CONFIGURE_PWD=changeme
-
+export CM_CONFIGURE_LIBEXECDIR=/opt1/cm/configure-libexec
+export BASH_PATH=/bin/bash
